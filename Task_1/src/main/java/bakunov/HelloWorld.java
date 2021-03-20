b5d94b73d9023372644e7afff1bc1ef2fd265bb1
Victor Khoroshev <victor@khoroshev.net>
2021-03-20 11:44:29 +0600

    Added first task.

--- Task_1/src/main/java/bakunov/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package bakunov;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
--- Task_1/src/main/java/prikhozhaya/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package prikhozhaya;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
