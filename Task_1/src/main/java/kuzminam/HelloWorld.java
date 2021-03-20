a06b2bc3787ca42175f0a98c39183697af516ead
MariiaKuzmina <mashuladeg@gmail.com>
2021-03-20 15:34:58 +0700

    First lesson added

---
+++ Task_1/src/main/java/kuzminam/HelloWorld.java
@@ -0,0 +1,14 @@
+package kuzminam;
+
+public class HelloWorld {
+    public void test() {
+        Printer ira = new Printer();
+        ira.hello("Hello Ira");
+        Printer.hello2("Hello Bro");
+    }
+
+    public static void main(String... args){
+        HelloWorld a = new HelloWorld();
+        a.test();
+    }
+}
