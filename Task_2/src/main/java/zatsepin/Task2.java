4a3af0813b84cbf84ca4c7d98ef31926fb2b49a8
BruteForce42 <zatsepinevgeniy@gmail.com>
2021-03-21 16:17:20 +0600

    Second task is done.

---
+++ Task_2/src/main/java/zatsepin/Task2.java
@@ -0,0 +1,23 @@
+package zatsepin;
+
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        double[] numbers = new double[10];
+        double sum = 0.0;
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = Math.random();
+        }
+        System.out.println("The array is filled with 10 pseudo-random numbers.");
+        Arrays.sort(numbers);
+        System.out.println("Minimum number in an array: " + String.format("%.2f", numbers[0]));
+        System.out.println("Maximum number in an array: " + String.format("%.2f", numbers[9]));
+
+        for (int i = 0; i < 10; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Average value in an array : " + String.format("%.2f", sum / numbers.length));
+    }
+}
