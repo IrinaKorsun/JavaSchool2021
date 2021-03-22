0417124c8c6d7583133aa972809a6ee0822a5bcf
ValentinLesnoy <89609989900@yandex.ru>
2021-03-22 07:06:57 +0200

    Task 2 is done.

---
+++ Task_2/src/main/java/lesnoy/Task2.java
@@ -0,0 +1,20 @@
+package lesnoy;
+
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+        double[] myArray = new double[20];
+
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = (double) Math.random();
+        }
+
+        double max = Arrays.stream(myArray).max().getAsDouble();
+        double min = Arrays.stream(myArray).min().getAsDouble();
+        double average = Arrays.stream(myArray).average().getAsDouble();
+
+        System.out.println("Max.value: " + max + "; \nMin.value: " + min + "; \nAverage.value: " + average);
+
+    }
+}
