ae84047e48809295bfeb1e938b55c161d40844d3
KrisGol <56467721+KrisGol@users.noreply.github.com>
2021-03-22 18:55:02 +0600

    Первое задание, выводит строку в консоль

---
+++ Task_1/src/main/java/Podolyak/MyString.java
@@ -0,0 +1,17 @@
+package Podolyak;
+
+public class MyString {
+    private String outString;
+
+    public MyString(String outString) {
+        this.outString = outString;
+    }
+
+    public void setOutString(String outString) {
+        this.outString = outString;
+    }
+
+    public void ourString(){
+        System.out.println(outString);
+    }
+}
