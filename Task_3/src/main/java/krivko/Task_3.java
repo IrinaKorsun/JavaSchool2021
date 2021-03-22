c2c09a10af944fa82fd7ea05dddb5a8051b64a1f
Николай Кривко <krivko_kod@mail.ru>
2021-03-22 21:46:09 +0600

    Добавлено 3е задание (калькулятор - Scanner)

---
+++ Task_3/src/main/java/krivko/Task_3.java
@@ -0,0 +1,51 @@
+package krivko;
+
+import java.util.Scanner;
+
+/**
+ * Задание Task_3
+ * Написать консольный калькулятор, умеющий производить базовые вычисления
+ * @author krivko
+ * @since 22.03.2021
+ * @version 1
+ */
+public class Task_3 {
+    public static void main(String... agr ) {
+
+        double a, b, result;
+        String operator;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" -- Калькулятор простых бинарных операций (+ - / * %)");
+        System.out.println(" -- Введите данные в формате a * b и нажмите на Enter");
+        System.out.println(" -- Внимание! Всё что введете после b учитываться не будет");
+        System.out.println(" -- Для выхода наберите символ Q и нажмите Enter");
+
+        while (!scanner.hasNext("[Qq]")) {
+            if (scanner.hasNextDouble()) {
+                a = scanner.nextDouble();
+                if (scanner.hasNext("[+-/*/%]")) {
+                    operator = scanner.next();
+                    if (scanner.hasNextDouble()) {
+                        b = scanner.nextDouble();
+                        switch (operator) {
+                            case "+" -> result = a + b;
+                            case "-" -> result = a - b;
+                            case "*" -> result = a * b;
+                            case "/" -> result = a / b;
+                            case "%" -> result = a % b;
+                            default -> result = 0;
+                        }
+                        System.out.printf("%.2f %s %.2f = %.2f\n", a, operator, b, result);
+                        scanner.nextLine();
+                        continue;
+                    }
+                }
+            }
+            System.out.println(" -- У вас явно что то не получилось! Попробуйте ещё раз (или наберите Q и нажмите Enter для выхода)");
+            scanner.nextLine();
+        }
+        System.out.println(" -- Поздравляем! Вам удалось выбраться из калькулятора.");
+        scanner.close();
+    }
+}
