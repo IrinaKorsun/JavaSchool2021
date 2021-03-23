611aa42af5ca861e798c9f9f5f7bd88b14c60b12
KrisGol <56467721+KrisGol@users.noreply.github.com>
2021-03-23 09:55:23 +0600

    Оставлен один класс

--- Task_1/src/main/java/Podolyak/Main.java
+++
@@ -1,8 +0,0 @@
-package Podolyak;
-
-public class Main {
-    public static void main (String[] args){
-        MyString myString = new MyString("Hello world!");
-        myString.ourString();
-    }
-}
