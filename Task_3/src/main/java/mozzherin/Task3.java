4ca86b5b9ef81e075a72ce8bde03e6b410ba7060
Mozzherin_AF <alexmozzherin91@gmail.com>
2021-03-24 11:44:47 +0600

    Home work on task 3

--- Task_3/src/main/java/mozzherin/Task3.java
+++ Task_3/src/main/java/mozzherin/Task3.java
@@ -1,7 +1,18 @@
 package mozzherin;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Calc calc = new Calc();
+        Scanner input = new Scanner(System.in);
 
+        while (true) {
+            String[] expression = input.nextLine().split(" ");
+
+            calc.setFirstVal(Double.parseDouble(expression[0]));
+            calc.setSecondVal(Double.parseDouble(expression[2]));
+            calc.runOperation(expression[1].charAt(0));
+        }
     }
 }
