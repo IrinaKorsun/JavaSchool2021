621bd6f575de0d233874df7edfe1eee809ab1c53
IrinaKorsun <irina.korsun.nsu@gmail.com>
2021-03-26 16:03:17 +0300

    added 4 task

--- Task_2/src/main/java/kuzmina/Task2.java
+++ Task_2/src/main/java/kuzmina/Task2.java
@@ -1,4 +1,44 @@
 package kuzmina;
 
 public class Task2 {
+    public static void main(String[] args) {
+        String s = "VvvveeeeeeeeeerrrrrryyyyyyyyyyyyyyyyyyyyyyyyyyyyLooooooooooooooooooooooo" +
+                "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+                "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+                "oooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+                "ooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+                "ooooooooooooooooooooooooooooonnnnngggSssttrrriiinnnnnggggggg";
+
+
+        long startTime = System.nanoTime();
+        calculateHash(s);
+        long estimatedTime = System.nanoTime() - startTime;
+        System.out.println("That took " + (estimatedTime) + " nanosecconds");
+
+
+        startTime = System.nanoTime();
+        calculateHash(s);
+        estimatedTime = System.nanoTime() - startTime;
+        System.out.println("That took " + (estimatedTime) + " nanosecconds");
+
+
+        startTime = System.nanoTime();
+        calculateHash(s);
+        estimatedTime = System.nanoTime() - startTime;
+        System.out.println("That took " + (estimatedTime) + " nanosecconds");
+
+
+        startTime = System.nanoTime();
+        calculateHash(s);
+        estimatedTime = System.nanoTime() - startTime;
+        System.out.println("That took " + (estimatedTime) + " nanosecconds");
+
+
+    }
+
+    private static int calculateHash(String s) {
+        return s.hashCode();
+    }
+
+
 }
