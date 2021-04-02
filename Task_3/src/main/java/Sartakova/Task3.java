9956ff4d5fa9ebcb8e5fef9a497cb509574beaf5
18542299 <OESartakova@sberbank.ru>
2021-04-03 00:30:04 +0700

    Исправления по 3 заданию

---
+++ Task_3/src/main/java/Sartakova/Task3.java
@@ -0,0 +1,58 @@
+package Sartakova;
+import java.util.Scanner;
+
+public class Task3 {
+    static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double a;
+        double b;
+        char operation;
+        double result;
+        System.out.println("Введите числа в виде: a+b/a-b/a*b/a:b ");
+
+        while (s.hasNext()) {
+            if (s.hasNextDouble()) {
+                a = s.nextDouble();
+            } else {
+                printError();
+                continue;
+            }
+            if (s.hasNext("[+-/*/]")) {
+                operation = s.next().charAt(0);
+            } else {
+                printError();
+                continue;
+            }
+            if (s.hasNextDouble()) {
+                b = s.nextDouble();
+            } else {
+                printError();
+                continue;
+            }
+
+            switch (operation) {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                default:
+                    result = 0;
+            }
+            System.out.println("Результат =  " + result);
+        }
+    }
+
+    private static void printError() {
+        System.out.println("Ошибка при вводе!");
+        s.next();
+    }
+}
