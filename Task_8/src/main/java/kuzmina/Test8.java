85acdd630a1eed23aab50163f7a4779b08ca37f2
Кузьмина Ирина Андреевна <korsun.i.an@sberbank.ru>
2021-04-20 14:29:18 +0700

    added 8 module

--- Task_8/src/main/java/kuzmina/Test8.java
+++ Task_8/src/main/java/kuzmina/Test8.java
@@ -1,4 +1,26 @@
 package kuzmina;
 
 public class Test8 {
+    public static void main(String[] args) {
+        byte a = 1;
+        byte b = 127;
+       // byte c = a + b;
+
+
+
+        float pi = (float) Math.PI;
+        double piDoub = Math.PI;
+        System.out.println(pi);
+        System.out.println(piDoub);
+
+        float amount1 = 1.000005F;
+        float amount2 = 0.000004F;
+        System.out.println(amount1 - amount2);
+
+        char symbol = '\u0066';
+        System.out.println(symbol);
+        symbol = 102;
+        System.out.println(symbol);
+    }
+
 }
